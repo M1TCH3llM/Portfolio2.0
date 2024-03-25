ec71c1c8d299f76e2c94094c000cc26f74c10a57
Mitchell.Morgan <mitchemorgan@gmail.com>
2024-03-25 17:41:52 -0600

    home page

---
+++ src/Pages/Home/home.js
@@ -0,0 +1,25 @@
+import React from "react";
+import './home.css';
+
+function home() {
+  return (
+    
+      <div id='home'>
+        <div></div>
+        <div>
+          <div></div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div></div>
+      </div>
+    
+  );
+}
+
+export default home;
