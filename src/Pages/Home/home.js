7e0ef9a9011f4aae20448286a3d3cfbb05f08237
Mitchell.Morgan <mitchemorgan@gmail.com>
2024-03-25 21:25:44 -0600

    lay out

--- src/Pages/Home/home.js
+++ src/Pages/Home/home.js
@@ -1,24 +1,42 @@
 import React from "react";
-import './home.css';
+import "./home.css";
 
 function home() {
   return (
-    
-      <div id='home'>
-        <div></div>
-        <div>
-          <div></div>
-          <div></div>
-        </div>
-        <div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-        </div>
-        <div></div>
+    <div id="home">
+      <div className="bigBox">
+        <h2>About</h2>
       </div>
-    
+      <div className="hidenBox">
+        <div>
+          <h2>Developer Projects</h2>
+        </div>
+        <div>
+          <h2>Ux Design Projects</h2>
+        </div>
+      </div>
+      <div className="hidenBox">
+        <div>
+          <h2>Contact</h2>
+        </div>
+        <div>
+          <h2>Linked In</h2>
+        </div>
+        <div>
+          <h2>Spotify</h2>
+        </div>
+        <div>
+          <h2>Git Hub</h2>
+        </div>
+      </div>
+      <div className="bigBox">
+        <h2>Reasons To Hire Me</h2>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </div>
+    </div>
   );
 }
 
